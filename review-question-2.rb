cf0f1b34aca37424f60efe4bbd0321a4f503e766
Jason Decastro <jasonrdecastro@gmail.com>
2016-07-29 14:55:59 -0400

    finished second one

--- review-question-2.rb
+++ review-question-2.rb
@@ -1,24 +1,50 @@
+require 'pry'
+
 class Car
+  attr_reader :make, :model
 
+  @@all = []
 
+  def initialize(model: model, make: make)
+    @make = make
+    @model = model
+    binding.pry
+    @@all << self
+  end
+
+  def make
+    @make[:make]
+  end
+
+  def model
+    @model[:model]
+  end
+
+  def drive
+    "VROOOOOOOOOOOOM!"
+  end
+
+  def self.all
+    @@all
+  end
 
 end
 
-car = Car.new("volvo", "lightening")
-car.make
-#=> "volvo"
-car.model
-#=> "ligthening"
+# car = Car.new("volvo", "lightening")
+# car.make
+# #=> "volvo"
+# car.model
+# #=> "ligthening"
 
-car.drive
-# => "VROOOOOOOOOOOOM!"
+# car.drive
+# # => "VROOOOOOOOOOOOM!"
 
-Car.all
+# Car.all
 #=> [car1, car2, car3]
 
-BONUS:
+# BONUS:
 
-Car.new(make: "volvo", model: "lightening")
+car = Car.new(make: "volvo", model: "lightening")
 
 car.make
 #=> "volvo"
