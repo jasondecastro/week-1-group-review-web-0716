34498038336c727ddb63b33bb378ad03a6e3f588
Jason Decastro <jasonrdecastro@gmail.com>
2016-07-29 14:24:28 -0400

    finished first one

--- review-question-1.rb
+++ review-question-1.rb
@@ -22,7 +22,7 @@ pokemon = [
 },
 {
   "id": 3,
-  "name": "venesaur",
+  "name": "venusaur",
   "base_experience": 50,
   "height": 10,
   "is_default": true,
@@ -61,9 +61,24 @@ pokemon = [
 ]
 
 
+
+
 # How would you get the url for Bulbasaur's ability?
+puts pokemon[0][:abilities][0][:ability][:url]
 # How would you return the first pokemon with base experience over 40?
+pokemon.detect do |pokemons|
+  pokemons[:base_experience] > 40
+end
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
+pokemon.select do |pokemons|
+  pokemons[:base_experience] > 40
+end
 # How would you return an array of all of the pokemon's names?
+pokemon.collect do |pokemons|
+  pokemons[:name]
+end
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
 #  whatever method you use should return true if there are any such pokemon, false if not.
+pokemon.any? do |pokemons|
+  pokemons[:weight] > 60
+end
